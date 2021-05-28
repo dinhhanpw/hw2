9ea445edd55b11074793bb24f5b12204c2acd0c0
dinhhanpw <dinhhan26031999@gmail.com>
2021-05-28 20:07:19 +0700

    test case 1-15

---
+++ Scripts/Test Case 3/Script1622200014794.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://deloyweb.herokuapp.com/signup_email?id=qznkk1ofgjyndbpkqsyg3mxcah&fbclid=IwAR07I26kIi8a4wvhACX1Ty5ujCJX7ZVB3kuvx48dbsUg8xbNHHv_crAyzgY')
+
+WebUI.setText(findTestObject('Object Repository/Mattermost_signup/input_email'), 'tester')
+
+WebUI.setText(findTestObject('Object Repository/Mattermost_signup/input_username'), 'super_tester')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Mattermost_signup/input_password'), 'qj1BTyeJoE60NzCSyvy8/Q==')
+
+WebUI.click(findTestObject('Object Repository/Mattermost_signup/button_Create Account'))
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/Mattermost_signup/input_email'), 10)
+
+WebUI.closeBrowser()
---
+++ Test Suites/Run all test cases.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run all test cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>93a044aa-0690-4238-aa1c-91a9d2be14b7</testSuiteGuid>
+</TestSuiteEntity>
