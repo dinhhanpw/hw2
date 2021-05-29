fc214d874c79d00692dc3a357f60c6bdfde5106e
quocdung1999 <1712369@student.hcmus.edu.vn>
2021-05-29 13:35:22 +0700

    Test Case 35

--- Test Suites/Run all test cases.ts
+++ Test Suites/Run all test cases.ts
@@ -11,4 +11,37 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>93a044aa-0690-4238-aa1c-91a9d2be14b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>51f5870d-e492-47a8-975f-2ad03f10148c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Data Driven Test Cases/Test Case 35</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>12eaeada-5daf-44ba-b6d8-28bfeab0563a</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/CSV Create Account</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>12eaeada-5daf-44ba-b6d8-28bfeab0563a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>fa21f7cf-9076-43e2-974a-1f3b6e745062</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>12eaeada-5daf-44ba-b6d8-28bfeab0563a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>7927d75e-0cb9-405a-a672-1e8a702c78d3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>12eaeada-5daf-44ba-b6d8-28bfeab0563a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>09721fc7-8804-4482-aad0-d877855e107d</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
